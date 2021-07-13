9e45f5095cf8e386610c0dfcbdc1866ce57d4f1d
kate <katecr17@gmail.com>
2021-07-13 07:36:33 -0500

    Create and navigate to customer view with tables

--- POSUNO/POSUNO/POSUNO.Shared/Pages/MainPage.xaml.cs
+++ POSUNO/POSUNO/POSUNO.Shared/Pages/MainPage.xaml.cs
@@ -20,6 +20,7 @@ namespace POSUNO.Pages
             base.OnNavigatedTo(e);
             User = (User)e.Parameter;
             WelcomeTextBlock.Text = $"Bienvenid@: {User.FullName}";
+            MyFrame.Navigate(typeof(CustomersPage));
         }
 
         private async void LogoutImage_Tapped(object sender, TappedRoutedEventArgs e)
@@ -42,6 +43,16 @@ namespace POSUNO.Pages
             };
 
             return await confirmDialog.ShowAsync();
+        }
+
+        private void CustomersNavigationViewItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(CustomersPage));
+        }
+
+        private void ProductsNavigationViewItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(ProductsPage));
         }
     }
 }
